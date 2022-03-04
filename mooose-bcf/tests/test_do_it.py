dc45cb37a888db017936c3c7d4bbce52b0ce8706
Murray Tait <murray.tait@infinityworks.com>
2022-03-04 17:04:30 +0000

    remove old test file

--- mooose-bcf/tests/test_do_it.py
+++
@@ -1,8 +0,0 @@
-from do_it import convert
-
-
-def test_convert():
-    df = convert(
-        "data/activity_187550144.gpx",
-        "build/test-reports/output.csv")
-    assert df.shape == (1509, 6)
