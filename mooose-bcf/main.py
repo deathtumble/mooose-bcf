a2adfcee3c2027d56527c9eb90a3d387c63c3875
Murray Tait <murray.tait@infinityworks.com>
2022-03-04 11:12:35 +0000

    Add some logging

--- mooose-bcf/main.py
+++ mooose-bcf/main.py
@@ -9,10 +9,16 @@ def hello_gcs(event, context):
     file_name = event['name']
     bucket_name = event['bucket']
 
+    print(f"bucket: {bucket_name}, file_name: {file_name}")
+
     extension = path.splitext(path.basename(file_name))[1]
 
     if extension == ".gpx":
         contents = read_file(bucket_name, file_name)
         activity_id = file_name.split("_")[1].split(".")[0]
+        print("inserting gpx file")
         rows = read_gpx_file(activity_id, contents)
         put_rows(rows, "a-cloud-guru-trial", "bcf", "gpx")
+        print("inserted gpx file")
+    else:
+        print("file ignored")
